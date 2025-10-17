8a8428efdf7efc59e1d8694f4dab94816c04213b
Abhilash3045 <dopatiabhilash@gmail.com>
2025-10-17 23:21:46 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (99.85%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.py
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.py
@@ -4,12 +4,8 @@ class Solution(object):
         :type num: str
         :rtype: str
         """
-        res=[]
-        for i in range(len(num)-2):
-            if num[i]==num[i+1]==num[i+2]:
-                res+=[num[i:i+3]]
-        if res:
-            x=str(max(list(map(int, res))))
-            return "000" if x=="0" else x
-        else:
-            return ""
+        res=['999','888','777','666','555','444','333','222','111','000']
+        for i in res:
+            if i in num:
+                return i
+        return ""
