33d7d7b6335f67ae5ddd1ce79ada6fcea76cf4c4
Abhilash <dopatiabhilash@gmail.com>
2025-10-19 23:24:46 +0530

    Time: 24 ms (24.38%) | Memory: 44.7 MB (32.33%) - LeetSync

---
+++ 415-add-strings/add-strings.cs
@@ -0,0 +1,8 @@
+public class Solution {
+    public string AddStrings(string num1, string num2) {
+        BigInteger x = BigInteger.Parse(num1);
+        BigInteger y = BigInteger.Parse(num2);
+        BigInteger z = x + y;
+        return z.ToString();
+    }
+}
