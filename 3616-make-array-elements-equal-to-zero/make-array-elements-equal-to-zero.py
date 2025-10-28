137d4f0011510d3ea186bce9e42c4709e5a55151
Abhilash <dopatiabhilash@gmail.com>
2025-10-28 15:26:57 +0530

    Time: 42 ms (90.96%) | Memory: 17.7 MB (79.79%) - LeetSync

--- 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.py
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.py
@@ -2,7 +2,7 @@ class Solution:
     def countValidSelections(self, nums: List[int]) -> int:
         t=sum(nums)
         p, a = 0, 0
-        for x in nums:
+        for i,x in enumerate(nums):
             s=t-p-x
             if x==0:
                 if p==s:
