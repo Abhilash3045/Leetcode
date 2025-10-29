c8839aa54c18458b6a47a72b0eb5ec20fa1120b2
Abhilash <dopatiabhilash@gmail.com>
2025-10-29 16:06:09 +0530

    Time: 1 ms (32.03%) | Memory: 12.4 MB (73.12%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -10,19 +10,35 @@ class Solution(object):
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        combo=[]
-        curr=list1
-        while curr:
-            combo.append(curr.val)
-            curr=curr.next
-        curr=list2
-        while curr:
-            combo.append(curr.val)
-            curr=curr.next
-        combo.sort()
-        dummy=ListNode(0)
+        # combo=[]
+        # curr=list1
+        # while curr:
+        #     combo.append(curr.val)
+        #     curr=curr.next
+        # curr=list2
+        # while curr:
+        #     combo.append(curr.val)
+        #     curr=curr.next
+        # combo.sort()
+        # dummy=ListNode(0)
+        # curr=dummy
+        # for i in combo:
+        #     curr.next=ListNode(i)
+        #     curr=curr.next
+        # return dummy.next
+        
+        dummy=ListNode()
         curr=dummy
-        for i in combo:
-            curr.next=ListNode(i)
+        while list1 and list2:
+            if list1.val<list2.val:
+                curr.next=list1
+                list1=list1.next
+            else:
+                curr.next=list2
+                list2=list2.next
             curr=curr.next
+        if list1:
+            curr.next=list1
+        if list2:
+            curr.next=list2
         return dummy.next
