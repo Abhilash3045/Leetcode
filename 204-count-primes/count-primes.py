23427b3a4893fd40e06575455f3347016032428d
Abhilash <dopatiabhilash@gmail.com>
2025-11-10 21:24:12 +0530

    Time: 2510 ms (50.37%) | Memory: 130.4 MB (90.38%) - LeetSync

---
+++ 204-count-primes/count-primes.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def countPrimes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n<=2: return 0
+        primes=[True]*n
+        primes[0]=primes[1]=False
+        p=2
+        while p*p<n:
+            if primes[p]:
+                for i in range(p*p,n,p):
+                    primes[i]=False
+            p+=1
+        return sum(primes)
