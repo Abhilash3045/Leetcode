86068ff0cf296489d28e8ad0a8446baa408bc67b
Abhilash <dopatiabhilash@gmail.com>
2025-11-19 22:36:36 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (77.18%) - LeetSync

--- 748-largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.py
+++ 748-largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.py
@@ -1,4 +1,11 @@
 class Solution:
     def dominantIndex(self, nums: List[int]) -> int:
-        num=sorted(nums)
-        return nums.index(num[-1]) if num[-1]>=2*num[-2] else -1
+        m1=0;m2=0;ind=0
+        for i in range(len(nums)):
+            if nums[i]>m1:
+                m2=m1
+                m1=nums[i]
+                ind=i
+            elif nums[i]>m2:
+                m2=nums[i]
+        return ind if m1>=2*m2 else -1
