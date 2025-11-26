5add612e37fbfa78b87d7429a6a48170278ef6d9
Abhilash <dopatiabhilash@gmail.com>
2025-11-26 23:03:37 +0530

    Time: 0 ms (100.00%) | Memory: 49 MB (90.64%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cs
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cs
@@ -1,9 +1,14 @@
 public class Solution {
     public IList<bool> KidsWithCandies(int[] candies, int extraCandies) {
         List<bool> res = new List<bool>();
-        int m = candies.Max();
+        int max = 0;
         foreach(int i in candies){
-            if(i+extraCandies>=m){
+            if(i>max){
+                max=i;
+            }
+        }
+        foreach(int i in candies){
+            if(i+extraCandies>=max){
                 res.Add(true);
             }
             else{
