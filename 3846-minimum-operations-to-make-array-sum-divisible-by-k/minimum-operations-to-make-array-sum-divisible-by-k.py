4e49459a3c2d81ff092fe35f3706ef506efeef03
Abhilash <dopatiabhilash@gmail.com>
2025-11-29 20:42:03 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (8.74%) - LeetSync

--- 3846-minimum-operations-to-make-array-sum-divisible-by-k/minimum-operations-to-make-array-sum-divisible-by-k.py
+++ 3846-minimum-operations-to-make-array-sum-divisible-by-k/minimum-operations-to-make-array-sum-divisible-by-k.py
@@ -1,8 +1,7 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
         t=sum(nums)
-        op=0
         if t%k==0:
-            return op
+            return 0
         else:
             return t%k
