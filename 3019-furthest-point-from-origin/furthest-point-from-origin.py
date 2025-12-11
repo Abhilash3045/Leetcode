990e3658a6b23b7458d139511b78aad7144e8218
Abhilash <dopatiabhilash@gmail.com>
2025-12-11 22:20:22 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (84.87%) - LeetSync

---
+++ 3019-furthest-point-from-origin/furthest-point-from-origin.py
@@ -0,0 +1,11 @@
+class Solution:
+    def furthestDistanceFromOrigin(self, moves: str) -> int:
+        l=0;r=0;d=0
+        for i in moves:
+            if i=='L': l+=1
+            elif i=='R': r+=1
+            else: d+=1
+        if l>=r:
+            return (l+d)-r
+        else:
+            return (r+d)-l
