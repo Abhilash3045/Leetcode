90695712bb0a462cb2970f5a43cd480775debf7f
Abhilash <dopatiabhilash@gmail.com>
2025-12-21 20:53:01 +0530

    Time: 15 ms (88.15%) | Memory: 44.4 MB (18.15%) - LeetSync

---
+++ 1511-count-number-of-teams/count-number-of-teams.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numTeams(int[] rating) {
+        int n = rating.length, res = 0;
+        for(int j=0; j<n; j++){
+            int ls=0,ll=0,rs=0,rl=0;
+            for(int i=0; i<j; i++){
+                if(rating[i]>rating[j]) ll++;
+                else    ls++;
+            }
+            for(int k=j+1; k<n; k++){
+                if(rating[k]>rating[j]) rl++;
+                else    rs++;
+            }
+            res+=(ls*rl)+(ll*rs);
+        }
+        return res;
+    }
+}
