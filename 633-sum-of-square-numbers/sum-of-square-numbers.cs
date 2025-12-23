ce4186ec849bb04ce3458b7cb3af91cea96f7897
Abhilash <dopatiabhilash@gmail.com>
2025-12-23 21:30:39 +0530

    Time: 1 ms (100.00%) | Memory: 31.1 MB (51.67%) - LeetSync

---
+++ 633-sum-of-square-numbers/sum-of-square-numbers.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public bool JudgeSquareSum(int c) {
+        for(int div=2; div*div<=c; div++){
+            if(c%div==0){
+                int ec=0;
+                while(c%div==0){
+                    ec++;
+                    c/=div;
+                }
+                if(div%4==3 && ec%2!=0) return false;
+            }
+        }
+        return c%4!=3;
+    }
+}
