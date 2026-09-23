f6412e2d8e45d64ce9d7196a8b0cc42c096433af
Abhilash <dopatiabhilash@gmail.com>
2026-09-23 20:44:41 +0530

    Time: 3 ms (21.92%) | Memory: 67.4 MB (23.29%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cs
+++ 2634-minimum-common-value/minimum-common-value.cs
@@ -1,12 +1,38 @@
-public class Solution {
-    public int GetCommon(int[] nums1, int[] nums2) {
-        int n1=nums1.Length, n2=nums2.Length;
-        int l=0,r=0;
-        while(l<n1 & r<n2){
-            if(nums1[l]==nums2[r])  return nums1[l];
-            else if(nums1[l]>nums2[r])  r+=1;
-            else    l+=1;
+public class Solution
+{
+    public int GetCommon(int[] nums1, int[] nums2)
+    {
+        if (nums1.Length > nums2.Length)
+        {
+            int[] temp = nums1;
+            nums1 = nums2;
+            nums2 = temp;
         }
+
+        foreach (int x in nums1)
+        {
+            int l = 0;
+            int r = nums2.Length - 1;
+
+            while (l <= r)
+            {
+                int mid = (l + r) / 2;
+
+                if (nums2[mid] == x)
+                {
+                    return x;
+                }
+                else if (nums2[mid] > x)
+                {
+                    r = mid - 1;
+                }
+                else
+                {
+                    l = mid + 1;
+                }
+            }
+        }
+
         return -1;
     }
 }
