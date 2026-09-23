9756d9dd8500231e8c6ca481d0c0cf3fe23a4484
Abhilash <dopatiabhilash@gmail.com>
2026-09-23 20:43:14 +0530

    Time: 0 ms (100.00%) | Memory: 67.4 MB (23.29%) - LeetSync

---
+++ 2634-minimum-common-value/minimum-common-value.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int GetCommon(int[] nums1, int[] nums2) {
+        int n1=nums1.Length, n2=nums2.Length;
+        int l=0,r=0;
+        while(l<n1 & r<n2){
+            if(nums1[l]==nums2[r])  return nums1[l];
+            else if(nums1[l]>nums2[r])  r+=1;
+            else    l+=1;
+        }
+        return -1;
+    }
+}
